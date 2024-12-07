e027dc978e359f43afff7706310792c996018456
Simon Worku <143561285+SimonWorku1@users.noreply.github.com>
2024-12-06 16:58:31 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import Title from "./Title.js";
+import Entry from "./Entry.js";
+import Info from "./Info.js";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const pokemon = encodeURIComponent(name.toLowerCase());
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => setData(r))
+      .catch((e) => setData(`${e}`));
+  }, [name]);
+
+  return (
+    <div className="App">
+      <Title text="Poké Finder" />
+      <Entry action={setName} />
+      <Info name={name} data={data} />
+    </div>
+  );
+}
